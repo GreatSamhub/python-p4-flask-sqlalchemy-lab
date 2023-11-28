700c8c239a9b15b8b793ca04a763d8f6943b39a0
+Sam Thongo <+samuelthongo73@gmail.com>
2023-11-28 04:34:23 +0300

    flask

--- server/app.py
+++ server/app.py
@@ -19,15 +19,57 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.get(id)
+    if not animal:
+        return make_response('', 404)
+    response_data = f'''
+        <ul> 
+            <li>ID: {animal.id}</li>
+            <li>Name: {animal.name}</li>   
+            <li>Species: {animal.species}</li>
+            <li>Zookeeper: {animal.zookeeper.name}</li>
+            <li>Enclosure: {animal.enclosure.environment}</li>
+        </ul>
+    '''
+    return response_data
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.get(id)
+    if not zookeeper:
+        return make_response('', 404)
+    response_data = f'''
+        <ul>  
+            <li>ID: {zookeeper.id}</li>
+            <li>Name: {zookeeper.name}</li>  
+            <li>Birthday: {zookeeper.birthday}</li>
+            <li>Animals:
+                <ul>
+                    {"".join(f"<li>{animal.name} - {animal.species}</li>" for animal in zookeeper.animals)}
+                </ul>
+            </li>
+        </ul>
+    '''
+    return response_data
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.get(id)
+    if not enclosure:
+        return make_response('', 404)
+    response_data = f'''
+        <ul>    
+            <li>ID: {enclosure.id}</li>
+            <li>Environment: {enclosure.environment}</li>
+            <li>Open to visitors: {enclosure.open_to_visitors}</li>
+            <li>Animals:
+                <ul>
+                    {"".join(f"<li>{animal.name} - {animal.species}</li>" for animal in enclosure.animals)}
+                </ul>
+            </li>
+        </ul>
+    '''
+    return response_data
 
 
 if __name__ == '__main__':
